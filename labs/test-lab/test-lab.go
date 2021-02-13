6ace6afd62808be0dd21242dc598c16d7c9ae287
Katacoda Scenario <scenario@katacoda.com>
2021-02-13 04:39:16 +0000

    Solve test-lab
    Signed-off-by: Katacoda Scenario <scenario@katacoda.com>

--- labs/test-lab/test-lab.go
+++ labs/test-lab/test-lab.go
@@ -1,9 +1,22 @@
 package main
 
 import (
-	"fmt"
+    "fmt"
+    "os"
 )
 
 func main() {
-	fmt.Println("Welcome to the jungle")
+
+    if (len(os.Args) < 2) {
+      fmt.Println("[ERROR]: Please type name(s) as argument")
+    } else {
+      var msg = "Hello "
+      for _, i := range os.Args[1:] {
+        msg += i + " "
+      }
+      msg = msg[:len(msg)-1]
+      msg += ", Welcome to the jungle"
+      fmt.Println(msg)
+    }
+
 }
